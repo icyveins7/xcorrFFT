fa1894c0e539b33e69e013e700ad785289dbc77f
seomac <icyveins@hotmail.com>
2024-04-22 12:13:27 +0800

    add some basic cmake structure, not ready yet

--- include/IppXcorrFFT.h
+++ include/IppXcorrFFT.h
@@ -5,60 +5,64 @@
 #include <vector>
 #include <thread>
 
-// Container to perform the standard sliding product and fft (IPP's DFT)
-// Similar to the other things I had in commonMex previously
-class IppXcorrFFT_32fc
+// T: complex type, U: real type
+// This is so it's more generic and can be used with
+// std::complex-like types that encapsulate,
+// as well as IPP-like types that strictly define the complex type.
+template <typename T, typename U>
+class XcorrFFT
 {
 public:
-	IppXcorrFFT_32fc(const Ipp32fc* cutout, int cutoutlen, int num_threads, bool autoConj);
-	~IppXcorrFFT_32fc();
+    XcorrFFT(const T* cutout, int cutoutlen, int num_threads, bool autoConj);
+    ~XcorrFFT() = default;
 
-	// main runtime method
-	void xcorr(
-		const Ipp32fc* src,
-		const int srclen,
-		const int startIdx, 
-		const int endIdx, 
-		const int idxStep
-	);
+    // main runtime method
+    void xcorr(
+        const T* src,
+        const int srclen,
+        const int startIdx, 
+        const int endIdx, 
+        const int idxStep
+    );
 
-	void xcorr_array(
-		const Ipp32fc* src,
+    void xcorr_array(
+        const T* src,
         const int srclen,
         const int startIdx, 
         const int endIdx, 
         const int idxStep,
-		float *productpeaks,
-		int *freqlistinds,
-		int outputlength
-	);
+        float *productpeaks,
+        int *freqlistinds,
+        int outputlength
+    );
 
-	// output vectors
-	std::vector<float> m_productpeaks;
-	std::vector<int> m_freqlistinds;
+    // output vectors
+    std::vector<U> m_productpeaks;
+    std::vector<int> m_freqlistinds;
 
 private:
-	int m_cutoutlen;
-	int m_num_threads = 1;
+    int m_cutoutlen;
+    int m_num_threads = 1;
 
-    ippe::vector<Ipp32fc> m_cutout;
-	Ipp32f m_cutoutNormSq;
+    ippe::vector<T> m_cutout;
+    U m_cutoutNormSq;
 
-	// threads
-	std::vector<std::thread> m_threads;
+    // threads
+    std::vector<std::thread> m_threads;
 
-	// internal work method
-	void xcorr_thread(
-		const Ipp32fc* src,
-		const int srclen,
-		const int startIdx, 
-		const int endIdx, 
-		const int idxStep,
-		const int tIdx,
-		float *productpeaks,
-		int *freqlistinds
-	);
+    // internal work method
+    void xcorr_thread(
+        const T* src,
+        const int srclen,
+        const int startIdx, 
+        const int endIdx, 
+        const int idxStep,
+        const int tIdx,
+        float *productpeaks,
+        int *freqlistinds
+    );
 
-	int getOutputLength(int startIdx, int endIdx, int idxStep);
+    int getOutputLength(int startIdx, int endIdx, int idxStep);
+
 
 };
